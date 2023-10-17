09beee69e8bfda424facefbd4f4835fb125b8c22
Zayne Zack Mundo <71628563+Zayne67@users.noreply.github.com>
2023-10-17 15:29:59 +0400

    Update

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+        var menu = document.querySelector('#menu');
+
+        // Changine Image on Menu
+        function about() {
+        menu.src = 'img/menu-about.webp';
+        }
+        function project() {
+        menu.src = 'img/menu-project.webp';
+        }
+        function service() {
+        menu.src = 'img/menu-service.webp';
+        }
+        function contact() {
+        menu.src = 'img/menu-contact.webp';
+        }
---
+++ js/subdivison.js
@@ -0,0 +1,21 @@
+        var image = document.querySelector('#subdivision');
+        
+        // Changing Image on hover
+        function residential() {
+        image.src = 'img/actsub1.webp';
+        }
+        function commercial() {
+        image.src = 'img/actsub2.webp';
+        }
+        function hospitality() {
+        image.src = 'img/actsub3.webp';
+        }
+        function office() {
+        image.src = 'img/actsub4.webp';
+        }
+        function health() {
+        image.src = 'img/actsub5.webp';
+        }
+        function educational() {
+        image.src = 'img/actsub6.webp';
+        }
