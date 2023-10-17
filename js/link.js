7926026e8ab3a0fecc64a9441fbbe9fb8e6ad38f
Zayne Zack Mundo <71628563+Zayne67@users.noreply.github.com>
2023-10-17 15:29:52 +0400

    Update

---
+++ js/link.js
@@ -0,0 +1,8 @@
+// Link on Click
+function home(){
+    window.open('index.html', '_self')
+}
+
+document.querySelector('.img-link').addEventListener('click', function() {
+    window.open('index.html', '_self');
+});
---
+++ js/services.js
@@ -0,0 +1,11 @@
+    var image = document.querySelector('#subdivision');
+// Changing Image on hover
+        function architect() {
+        image.src = 'img/service1.webp';
+        }
+        function interior() {
+        image.src = 'img/service2.webp';
+        }
+        function pmc() {
+        image.src = 'img/service3.webp';
+        }
